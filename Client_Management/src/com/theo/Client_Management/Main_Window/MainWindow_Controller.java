102ba56d9ff7c63dd137fa3f0724b1d28aaaddd8
Theo <gmbaxevanos@gmx.ch>
2019-04-26 02:32:16 +0200

    v1.0.1

--- Client_Management/src/com/theo/Client_Management/Main_Window/MainWindow_Controller.java
+++ Client_Management/src/com/theo/Client_Management/Main_Window/MainWindow_Controller.java
@@ -1,4 +1,33 @@
 package com.theo.Client_Management.Main_Window;
 
-public class MainWindow_Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindow_Controller implements Initializable {
+
+    public void newClientButtonPushed(ActionEvent event) throws IOException {
+
+        Parent newClientScene = FXMLLoader.load(getClass().getResource("../New_Client_Window/New_Client.fxml"));
+
+        Scene addClient = new Scene(newClientScene,600,400);
+        Stage mainStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        mainStage.setScene(addClient);
+        mainStage.show();
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
--- Client_Management/src/com/theo/Client_Management/New_Client_Window/New_Client_Controller.java
+++ Client_Management/src/com/theo/Client_Management/New_Client_Window/New_Client_Controller.java
@@ -1,4 +1,32 @@
 package com.theo.Client_Management.New_Client_Window;
 
-public class New_Client_Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class New_Client_Controller implements Initializable {
+
+    public void backToMainWindowButtonPushed(ActionEvent event) throws IOException {
+
+        Parent newClientScene = FXMLLoader.load(getClass().getResource("../Main_Window/MainWindow.fxml"));
+
+        Scene addClient = new Scene(newClientScene,600,400);
+        Stage mainStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        mainStage.setScene(addClient);
+        mainStage.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
